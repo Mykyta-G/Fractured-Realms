8a33984144577ce3f29933f8b61c8f7ec538a054
YodaOnCrack <mykyta006@gmail.com>
2025-04-20 20:42:26 +0200

    Added main GameLoop

--- Fractured-Realms/src/Main.java
+++ Fractured-Realms/src/Main.java
@@ -17,6 +17,8 @@ public class Main {
 
             gameWindow.setLocationRelativeTo(null);
             gameWindow.setVisible(true);
+
+            gamePanel.startGameThread();
         });
     }
 }
