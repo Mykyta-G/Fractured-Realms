29a23a65dd586f7a95f50c4b9e2df1f7516acb4f
YodaOnCrack <mykyta006@gmail.com>
2025-04-27 16:04:47 +0200

    Tiles partly complete

--- Fractured-Realms/src/tile/TileManager.java
+++ Fractured-Realms/src/tile/TileManager.java
@@ -24,23 +24,28 @@ public class TileManager {
     public void getTileImage() {
 
         try {
-
+            System.out.println("Loading tiles...");
+            
             tile[0] = new Tile();
             tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/grass00.png"));
+            System.out.println("Grass tile loaded: " + (tile[0].image != null));
 
             tile[1] = new Tile();
             tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/wall.png"));
 
             tile[2] = new Tile();
-            tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/water.png"));
+            tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/water00.png"));
 
         }catch (IOException e){
+            System.out.println("Failed to load tile: " + e.getMessage());
             e.printStackTrace();
         }
     }
     public  void draw(Graphics2D g2){
 
         g2.drawImage(tile[0].image, 0, 0, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(tile[1].image, 48, 0, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(tile[2].image, 96, 0, gp.tileSize, gp.tileSize, null);
     }
 
 }
