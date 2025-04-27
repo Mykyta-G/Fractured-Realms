4cf9ea8f9b5b895093e001900b2ad4004b74e11d
YodaOnCrack <mykyta006@gmail.com>
2025-04-27 14:16:26 +0200

    IDK

--- Fractured-Realms/src/main/GamePanel.java
+++ Fractured-Realms/src/main/GamePanel.java
@@ -1,6 +1,7 @@
 package main;
 
 import entity.Player;
+import tile.TileManager;
 
 import javax.swing.*;
 import java.awt.*;
@@ -20,14 +21,10 @@ public class GamePanel extends JPanel implements Runnable{
     // FPS
     int FPS = 60;
 
+    TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     Player player = new Player(this,keyH);
-
-    //Set player's default position
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 4;
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHight));
@@ -82,6 +79,8 @@ public class GamePanel extends JPanel implements Runnable{
         super.paintComponent(g);
 
         Graphics2D g2  = (Graphics2D) g;
+
+        tileM.draw(g2);
 
         player.draw(g2);
 
